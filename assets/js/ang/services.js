454d0a25148338dd06983cb568a37076d8496def
Panos Louridas <louridas@gmail.com>
2014-05-08 15:16:52 +0300

    start testing (failing)

--- assets/js/ang/services.js
+++ assets/js/ang/services.js
@@ -1,4 +1,11 @@
 'use strict';
 
-angular.module('agoraApp.services', []).
+var agoraAppServices = angular.module('agoraAppServices', ['ngResource']).
   value('version', '0.1.0');
+
+agoraAppServices.factory('Provider', ['$resource',
+  function($resource) {
+    return $resource('providers/:providers.json', {}, {
+      query: {method:'GET', params:{providers:'providers'}, isArray:true}
+    });
+  }]);
