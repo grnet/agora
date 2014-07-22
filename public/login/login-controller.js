cb077da23a171018e36d206e29f7e9e8505b8f52
Panos Louridas <louridas@gmail.com>
2014-07-22 14:01:32 +0300

    sign in and sign out

--- public/login/login-controller.js
+++ public/login/login-controller.js
@@ -3,17 +3,17 @@
 var agoraAppLoginController =
   angular.module('agoraAppLoginController', []);
   
-agoraAppLoginController.controller('LoginCtrl', ['$scope', '$location',
-  '$window', 'Login',
-  function ($scope, $location, $window, Login) {
+agoraAppLoginController.controller('LoginCtrl', ['$scope', '$rootScope',
+  '$location', '$window', 'Login',
+  function ($scope, $rootScope, $location, $window, Login) {
     $scope.login = function(form) {
       Login.login({username: $scope.username, password: $scope.password},
         function(value, responseHeaders) {
-          console.log("JERE");
           $window.sessionStorage.token = value.token;
           $window.sessionStorage.firstName = value.firstName;
           $window.sessionStorage.surname = value.surname;
           $window.sessionStorage.username = value.username;
+          $rootScope.$broadcast('login', {user: value.username});
           $location.path('/');
         }, 
         function(httpResponse) {
--- public/user/user-controller.js
+++ public/user/user-controller.js
@@ -4,13 +4,30 @@ var agoraAppUserController =
   angular.module('agoraAppUserController', []);
 
 agoraAppUserController.controller('UserCtrl',
-  ['$rootScope', '$window', 
-  function($rootScope, $window) {
-    if ($window.sessionStorage.token) {
-      $rootScope.username = $window.sessionStorage.username;
-      $rootScope.firstName = $window.sessionStorage.firstName;
-      $rootScope.surname = $window.sessionStorage.surname;
-    }
+  ['$scope', '$window', 
+  function($scope, $window) {
+
+    $scope.$on('login', function(event, args) {
+      signIn();
+    });
+
+    var signIn = function() {
+      if ($window.sessionStorage.token) {
+        $scope.username = $window.sessionStorage.username;
+        $scope.firstName = $window.sessionStorage.firstName;
+        $scope.surname = $window.sessionStorage.surname;
+      }
+    };
+    
+    $scope.signOut = function() {
+      delete $window.sessionStorage.token;
+      delete $window.sessionStorage.username;
+      delete $window.sessionStorage.firstName;
+      delete $window.sessionStorage.surname;
+      delete $scope.username;
+      delete $scope.firstName;
+      delete $scope.surname;
+    };
     
   }
 ]);
