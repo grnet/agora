9880e82af2edf1a51d192bd7be635658f6b16836
Christos Kanellopoulos <skanct@gmail.com>
2014-10-29 15:01:40 +0200

    Fixes indentation

--- backend/routes/saml_metadata.js
+++ backend/routes/saml_metadata.js
@@ -5,15 +5,15 @@ var SamlStrategy = require('../lib/passport-saml-dynamic-idp').Strategy;
 
     
 router.get('/', function(req, res, next) {
-	var samlConfig = {
-        issuer: conf.passport.saml.issuer,
-        path: conf.passport.saml.path,
-        callbackUrl: conf.passport.saml.callbackUrl,
-        identifierFormat: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-      };
+  var samlConfig = {
+    issuer: conf.passport.saml.issuer,
+    path: conf.passport.saml.path,
+    callbackUrl: conf.passport.saml.callbackUrl,
+    identifierFormat: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+  };
   var strategy = new SamlStrategy( samlConfig, function() {} );
   res.set('Content-Type', 'text/xml');
-	res.send(strategy.generateServiceProviderMetadata( conf.passport.saml.decryptionCert ));
+  res.send(strategy.generateServiceProviderMetadata( conf.passport.saml.decryptionCert ));
 });
 
 module.exports = router;
