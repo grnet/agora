4a55b614de9b35c68a95a78c13e1e5eb11c38788
Panos Louridas <louridas@gmail.com>
2017-06-19 14:11:14 +0300

    always show navigation bar

--- public/main/main-controller.js
+++ public/main/main-controller.js
@@ -4,7 +4,5 @@ var agoraAppMainController =
   angular.module('agoraAppMainController', []);
 
 agoraAppMainController.controller('MainCtrl',
-  ['$scope', '$location',
-  function($scope, $location, currentPath) {
-    $scope.$parent.currentPath = $location.path();
-}]);
+  function() {
+});
